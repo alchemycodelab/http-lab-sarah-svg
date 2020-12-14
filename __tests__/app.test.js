0ed18538f264a062dd2bcb7f2bc8fadd9a170b4c
sarah-svg <kjskillen@gmail.com>
2020-12-14 15:24:52 -0800

    test get

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('should get spot', async() => {
+    const res = await request(app)
+      .get('/');
 
+    expect(res.text).toEqual('hi');
+  });
 });
