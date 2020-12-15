203072c67c2ae71f7773650320def575d843702a
sarah-svg <kjskillen@gmail.com>
2020-12-14 16:39:57 -0800

    test /red /blue /gree

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,4 +15,29 @@ describe('app routes', () => {
       .send('hi');
     expect(res.text).toEqual('hi');
   });
+
+  it('should GET red', async() => {
+    
+    const res = await request(app)
+      .get('/red');
+    
+    expect(res.text).toEqual('<h1>red</h1>');
+  });
+
+  it('should get red', async() => {
+    const res = await request(app)
+      .get('/green');
+     
+    expect(res.text).toEqual('<h1>green</h1>');
+  });
+
+  it('should get red', async() => {
+    const res = await request(app)
+      .get('/blue');
+  
+    expect(res.text).toEqual('<h1>blue</h1>');
+  });
+
+
+
 });
